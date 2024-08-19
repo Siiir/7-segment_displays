8d8249347ca6a36058a38229a8f3501b4bd037fa
= <tomasz_nehring@outlook.com>
2024-08-19 09:49:44 +0200

    the last update of the logic

--- 7-segment_displays.ino
+++ 7-segment_displays.ino
@@ -1,7 +1,9 @@
 #include "G:\My Drive\Open to public\IT\full-fledged\C, C++\Arduino\7-segment_displays\programs.inc"
 
-#define program jointCountUpTo9
-// #define NDEBUG
+// #define program ExperimentalCountUp
+#define program CountUpTo99999
+
+#define NDEBUG true
 
 void setup(){
   program::setup();
